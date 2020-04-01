ab47bc61cd90b7041ae9f27719e60ccb2b885d92
Tom Janssen <t.janssen13788@gmail.com>
2020-04-01 18:36:22 +0200

    ManagQuizzes scherm mooie opmaak, haalt nu alleen quizzes op die bij coordinator horen.

--- src/main/java/controller/CreateUpdateQuizController.java
+++ src/main/java/controller/CreateUpdateQuizController.java
@@ -4,7 +4,14 @@ import model.Quiz;
 
 public class CreateUpdateQuizController {
 
-    public void setup(Quiz quiz) {}
+    public void setup(Quiz quiz) {
+        titleLabel.setText(quiz.getName());
+        klantnummerTextfield.setText(String.valueOf(customer.getCustomerId()));
+        voorlettersTextfield.setText(customer.getInitials());
+        tussenvoegselTextfield.setText(customer.getPrefix());
+        achternaamTextfield.setText(customer.getSurName());
+        mobielTextfield.setText(customer.getMobilePhone());
+    }
 
     public void doMenu() {}
 
