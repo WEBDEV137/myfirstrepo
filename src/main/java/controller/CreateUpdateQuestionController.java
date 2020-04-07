44f832bd58bf798d38f9279dede4c49eb7848054
Joel Schuuring <joel.schuuring@gmail.com>
2020-04-07 09:11:35 +0200

    Merge

--- src/main/java/controller/CreateUpdateQuestionController.java
+++ src/main/java/controller/CreateUpdateQuestionController.java
@@ -145,83 +145,6 @@ public class CreateUpdateQuestionController extends AbstractController {
         setNewWrongAnswer3Answer();
     }
 
-/*    @FXML
-    private void createQuestion(){
-        String newQuestion = createUpdateQuestionTextField.getText();
-        if (newQuestion.isEmpty()) {
-            Alert nothingFilledIn = new Alert(Alert.AlertType.ERROR);
-            nothingFilledIn.setContentText("Je moet een vraag invullen.");
-            nothingFilledIn.show();
-            return;
-        }
-        this.newQuestion = new Question(newQuestion);
-    }
-
-    @FXML
-    private void createRightAnswer(){
-        String rightAnswer = createUpdateRightAnswerTextField.getText();
-        this.newRightAnswer = new Answer(rightAnswer, true);
-    }
-
-    @FXML
-    private void createWrongAnswer1(){
-        String newWrongAnswer = createUpdateWrongAnswerTextField1.getText();
-        this.newWrongAnswer1 = new Answer(newWrongAnswer, false);
-    }
-
-    @FXML
-    private void createWrongAnswer2(){
-        String newWrongAnswer = createUpdateWrongAnswerTextField2.getText();
-        this.newWrongAnswer2 = new Answer(newWrongAnswer, false);
-    }
-
-    @FXML
-    private void createWrongAnswer3(){
-        String newWrongAnswer = createUpdateWrongAnswerTextField3.getText();
-        this.newWrongAnswer3 = new Answer(newWrongAnswer, false);
-    }
-
-    @FXML
-    private void enableWrongAnswerTextfield2(){
-        createUpdateWrongAnswerTextField2.setDisable(false);
-    }
-
-    @FXML
-    private void enableWrongAnswerTextfield3(){
-        createUpdateWrongAnswerTextField3.setDisable(false);
-    }
-
-    @FXML
-    public void doCreateUpdateQuestionAndAnswers() {
-        createQuestion();
-        createRightAnswer();
-        createWrongAnswer1();
-        if (newRightAnswer.getText().isEmpty() || newWrongAnswer1.getText().isEmpty()){
-            Alert nothingFilledIn = new Alert(Alert.AlertType.ERROR);
-            nothingFilledIn.setContentText("Je moet minstens één juist en één fout antwoord invullen.");
-            nothingFilledIn.show();
-            return;
-        }
-        if (newWrongAnswer2 != null) {createWrongAnswer2();}
-        if (newWrongAnswer3 != null) {createWrongAnswer3();}
-        if (selectedQuestion == null) {
-            questionDAO.storeOne(newQuestion);
-            answerDAO.storeOne(newRightAnswer, 1, questionDAO.getIdByQuestionText(newQuestion.toString()));
-            answerDAO.storeOne(newWrongAnswer1, 0, questionDAO.getIdByQuestionText(newQuestion.toString()));
-            if (newWrongAnswer2 != null) {answerDAO.storeOne(newWrongAnswer2, 0, questionDAO.getIdByQuestionText(newQuestion.toString()));}
-            if (newWrongAnswer3 != null) {answerDAO.storeOne(newWrongAnswer3, 0, questionDAO.getIdByQuestionText(newQuestion.toString()));}
-            Alert stored = new Alert(Alert.AlertType.INFORMATION);
-            stored.setContentText("Vraag en antwoorden opgeslagen");
-            stored.show();
-        } else {
-            questionDAO.updateQuestionByQuestionText(newQuestion.toString(), selectedQuestion.toString());
-            Alert updated = new Alert(Alert.AlertType.INFORMATION);
-            updated.setContentText("Vraag en antwoorden gewijzigd");
-            updated.show();
-        }
-    }*/
-
-
     @FXML
     public void doBackToManage() {
         Main.getSceneManager().showManageQuestionsScene();
@@ -251,4 +174,178 @@ public class CreateUpdateQuestionController extends AbstractController {
 
     }
 
+
+    @FXML
+    private void enableWrongAnswerTextfield2(){
+        createUpdateWrongAnswerTextField2.setDisable(false);
+    }
+
+    @FXML
+    private void enableWrongAnswerTextfield3(){
+        createUpdateWrongAnswerTextField3.setDisable(false);
+    }
 }
+
+/*
+Joël's versie
+
+public class CreateUpdateQuestionController {
+    private QuestionDAO questionDAO = new QuestionDAO(Main.getDBaccess());
+    private AnswerDAO answerDAO = new AnswerDAO(Main.getDBaccess());
+    private QuizDAO quizDAO = new QuizDAO(Main.getDBaccess());
+    private Question newQuestion;
+    private Answer newRightAnswer;
+    private Answer newWrongAnswer1;
+    private Answer newWrongAnswer2;
+    private Answer newWrongAnswer3;
+    private Question selectedQuestion;
+    private Quiz selectedQuiz;
+    private String selectedQuizName;
+
+    @FXML
+    public Label titleLabel;
+    public Label answerLabel;
+    public TextField createUpdateRightAnswerTextField;
+    public TextField createUpdateWrongAnswerTextField1;
+    public TextField createUpdateWrongAnswerTextField2;
+    public TextField createUpdateWrongAnswerTextField3;
+    public TextField createUpdateQuestionTextField;
+    public Button createUpdateAnswerButton;
+    public MenuButton quizMenuButton;
+
+    public void setup(Question question) {
+        if (question != null){
+            titleLabel.setText("Wijzig vraag");
+            answerLabel.setText("Wijzig antwoorden");
+            createUpdateAnswerButton.setText("Wijzig vraag en antwoorden");
+            createUpdateQuestionTextField.setText(question.toString());
+            ArrayList<Answer> answerList = new ArrayList<>(answerDAO.getAllAnswersByQuestionId(question.getQuestionID()));
+            createUpdateRightAnswerTextField.setText(answerList.get(0).getText());
+            createUpdateWrongAnswerTextField1.setText(answerList.get(1).getText());
+            createUpdateWrongAnswerTextField2.setText(answerList.get(2).getText());
+            if (!createUpdateWrongAnswerTextField2.getText().isEmpty()){
+                createUpdateWrongAnswerTextField2.setDisable(false);
+            }
+            createUpdateWrongAnswerTextField3.setText(answerList.get(3).getText());
+            if (!createUpdateWrongAnswerTextField3.getText().isEmpty()){
+                createUpdateWrongAnswerTextField3.setDisable(false);
+            }
+            quizMenuButton.setText(quizDAO.getOneById(question.getQuizID()).getName());
+            selectedQuizName = quizDAO.getOneById(question.getQuizID()).getName();
+            selectedQuestion = question;
+        } fillQuizDropdownMenu();
+    }
+
+    @FXML
+    private void createQuestion(){
+        String newQuestion = createUpdateQuestionTextField.getText();
+        if (newQuestion.isEmpty()) {
+            Alert nothingFilledIn = new Alert(Alert.AlertType.ERROR);
+            nothingFilledIn.setContentText("Je moet een vraag invullen.");
+            nothingFilledIn.show();
+            return;
+        }
+        this.newQuestion = new Question(newQuestion);
+        if (selectedQuiz == null || selectedQuizName.isEmpty()){
+            Alert nothingFilledIn = new Alert(Alert.AlertType.ERROR);
+            nothingFilledIn.setContentText("Je moet een quiz selecteren.");
+            nothingFilledIn.show();
+            return;
+        }
+        this.newQuestion.setQuizID(selectedQuiz.getId());
+    }
+
+    @FXML
+    private void createRightAnswer(){
+        String rightAnswer = createUpdateRightAnswerTextField.getText();
+        this.newRightAnswer = new Answer(rightAnswer, true);
+    }
+
+    @FXML
+    private void createWrongAnswer1(){
+        String newWrongAnswer = createUpdateWrongAnswerTextField1.getText();
+        this.newWrongAnswer1 = new Answer(newWrongAnswer, false);
+    }
+
+    @FXML
+    private void createWrongAnswer2(){
+        String newWrongAnswer = createUpdateWrongAnswerTextField2.getText();
+        this.newWrongAnswer2 = new Answer(newWrongAnswer, false);
+    }
+
+    @FXML
+    private void createWrongAnswer3(){
+        String newWrongAnswer = createUpdateWrongAnswerTextField3.getText();
+        this.newWrongAnswer3 = new Answer(newWrongAnswer, false);
+    }
+
+    @FXML
+    public void doCreateUpdateQuestionAndAnswers() {
+        createQuestion();
+        if (newRightAnswer.getText().isEmpty() || newWrongAnswer1.getText().isEmpty()){
+            Alert nothingFilledIn = new Alert(Alert.AlertType.ERROR);
+            nothingFilledIn.setContentText("Je moet minstens één juist en één fout antwoord invullen.");
+            nothingFilledIn.show();
+            return;
+        }
+        createRightAnswer();
+        createWrongAnswer1();
+        if (newWrongAnswer2 != null) {createWrongAnswer2();}
+        if (newWrongAnswer3 != null) {createWrongAnswer3();}
+        if (selectedQuestion == null) {
+            newQuestion.setQuizID(selectedQuiz.getId());
+            questionDAO.storeOne(newQuestion);
+            newRightAnswer.setQuestionId(questionDAO.getIdByQuestionText(newQuestion.toString()));
+            answerDAO.storeOne(newRightAnswer);
+            newWrongAnswer1.setQuestionId(questionDAO.getIdByQuestionText(newQuestion.toString()));
+            answerDAO.storeOne(newWrongAnswer1);
+            if (newWrongAnswer2 != null) {
+                newWrongAnswer2.setQuestionId(questionDAO.getIdByQuestionText(newQuestion.toString()));
+                answerDAO.storeOne(newWrongAnswer2);}
+            if (newWrongAnswer3 != null) {
+                newWrongAnswer3.setQuestionId(questionDAO.getIdByQuestionText(newQuestion.toString()));
+                answerDAO.storeOne(newWrongAnswer3);}
+            Alert stored = new Alert(Alert.AlertType.INFORMATION);
+            stored.setContentText("Vraag en antwoorden opgeslagen");
+            stored.show();
+        } else {
+            questionDAO.updateQuestionByQuestionText(newQuestion.toString(), selectedQuestion.toString());
+            answerDAO.updateAnswerByAnswerText(newRightAnswer.toString(), createUpdateRightAnswerTextField.getText());
+            answerDAO.updateAnswerByAnswerText(newWrongAnswer1.toString(), createUpdateWrongAnswerTextField1.getText());
+            answerDAO.updateAnswerByAnswerText(newWrongAnswer2.toString(), createUpdateWrongAnswerTextField2.getText());
+            answerDAO.updateAnswerByAnswerText(newWrongAnswer3.toString(), createUpdateWrongAnswerTextField3.getText());
+            Alert updated = new Alert(Alert.AlertType.INFORMATION);
+            updated.setContentText("Vraag en antwoorden gewijzigd");
+            updated.show();
+        }
+    }
+
+    @FXML
+    private void fillQuizDropdownMenu (){
+        ArrayList<Quiz> quizList = quizDAO.getAll();
+        for (Quiz quiz :
+                quizList) {
+            MenuItem menuItem = new MenuItem(quiz.getName());
+            menuItem.setOnAction(actionEvent -> selectedQuiz = quiz);
+            //menuItem.setOnAction(actionEvent -> quizMenuButton.setText(quiz.getName()));
+            quizMenuButton.getItems().add(menuItem);
+        }
+    }
+
+    @FXML
+    private void enableWrongAnswerTextfield2(){
+        createUpdateWrongAnswerTextField2.setDisable(false);
+    }
+
+    @FXML
+    private void enableWrongAnswerTextfield3(){
+        createUpdateWrongAnswerTextField3.setDisable(false);
+    }
+
+    @FXML
+    public void doBackToManage() {
+        Main.getSceneManager().showManageQuestionsScene();
+    }
+
+    public void doMenu() { Main.getSceneManager().showWelcomeScene(Main.getCurrentUser());}
+}*/
