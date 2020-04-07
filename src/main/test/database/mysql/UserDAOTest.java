1f3078599c636319997c63bd882527048d328da8
Emrullah <emrullah.kilic@hva.nl>
2020-04-07 13:02:33 +0200

    update UserDAOTest

--- src/main/test/database/mysql/UserDAOTest.java
+++ src/main/test/database/mysql/UserDAOTest.java
@@ -1,5 +1,6 @@
 package database.mysql;
 
+import model.Quiz;
 import model.User;
 import org.junit.jupiter.api.Test;
 import view.Main;
@@ -39,11 +40,11 @@ class UserDAOTest {
 
     @Test
     void getOneById() {
-        DBAccess dbAccess = Main.getDBaccess();
+        /*DBAccess dbAccess = Main.getDBaccess();
         dbAccess.openConnection();
         UserDAO userDAO = new UserDAO(dbAccess);
-        assertNotNull(userDAO.getOneById(2));
-       /* DBAccess dbAccess = Main.getDBaccess();
+        assertNotNull(userDAO.getOneById(2));*/
+        DBAccess dbAccess = Main.getDBaccess();
         dbAccess.openConnection();
         UserDAO userDAO = new UserDAO(dbAccess);
         int id = 1;
@@ -76,11 +77,42 @@ class UserDAOTest {
         assertEquals(expecteduserNaam,actuelUserNaam);
         assertEquals(expectedAchternaam,actuelAchternaam);
         System.out.println(testUser);
-        System.out.println(actuelUser);*/
-
+        System.out.println(actuelUser);
     }
 
     @Test
     void storeOne() {
+        DBAccess dbAccess = Main.getDBaccess();
+        dbAccess.openConnection();
+        UserDAO userDAO = new UserDAO(dbAccess);
+        int id = 1;
+        String rol = "Student";
+        String userNaam = "Hans";
+        String wachtwoord = "12345";
+        String naam = "Hans";
+        String tussenvoegsels = "";
+        String achternaam = "Janssen";
+        User testUser = new User(id,rol,userNaam,wachtwoord,naam,tussenvoegsels,achternaam);
+        int expectedId = id;
+        String expectedRol = rol;
+        String expecteduserNaam = userNaam ;
+        String expectedWachtwoord = wachtwoord;
+        String expectedNaam = naam;
+        String expectedTussenvoegsels = tussenvoegsels;
+        String expectedAchternaam = achternaam;
+        User actuelUser = (User) userDAO.getOneById(expectedId);
+        int actuelId = actuelUser.getUserId();
+        String actuelUserNaam = actuelUser.getUserName();
+        String actuelRol = actuelUser.getRolName();
+        String actuelWachtwoord = actuelUser.getPassword();
+        String actuelNaam = actuelUser.getName();
+        String actuelTussenvoegsels = actuelUser.getPrefix();
+        String actuelAchternaam = actuelUser.getSurname();
+        assertEquals(expectedId,actuelId);
+        assertEquals(expectedRol,actuelRol);
+        assertEquals(expectedWachtwoord,actuelWachtwoord);
+        assertEquals(expectedNaam,actuelNaam);
+        assertEquals(expecteduserNaam,actuelUserNaam);
+        assertEquals(expectedAchternaam,actuelAchternaam);
     }
 }
