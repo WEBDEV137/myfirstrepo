7c7a2099b394ae06acaa9ec5cec4edc4a9a40249
minke <minke.pronk@upcmail.nl>
2020-04-08 11:21:25 +0200

    Code smells

--- src/main/test/model/CourseTest.java
+++ src/main/test/model/CourseTest.java
@@ -1,7 +1,26 @@
 package model;
 
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class CourseTest {
+/** testen van de getters van id en cursusnaam methoden van Course
+ */
 
-}
+class CourseTest extends TestCase {
+    private final Course Course_Test = new Course(2, "Testen", 15);
+
+    @Test
+    void testGetId() throws Exception {
+        assertEquals(Course_Test.getId(), 2);
+    }
+
+    @Test
+    void testGetCoursename() throws Exception{
+        assertEquals(Course_Test.getCoursename(),"Testen");
+    }
+
+
+  }
